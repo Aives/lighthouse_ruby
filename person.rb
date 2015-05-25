ae763b327ad43e821385064612cb85420eabd724
Aives <al.ives.1337@gmail.com>
2015-05-25 00:07:10 -0400

    comment out exercise, practiced a lil

--- person.rb
+++ person.rb
@@ -1,25 +1,25 @@
-class Person
-    attr_reader :first_name, :last_name, :gender
+#class Person
+#    attr_reader :first_name, :last_name, :gender
+#
+#    def initialize(first_name, last_name, gender)
+#        @first_name = first_name
+#        @last_name = last_name
+#        @gender = gender
+#    end
+#    def full_name
+#      "#{first_name} #{last_name}"
+#    end
+#    def doctors
+#      "Dr. #{full_name}"
+#    end
+#    def lawyers
+#      "#{full_name}, Esq."
+#    end
+#end
 
-    def initialize(first_name, last_name, gender)
-        @first_name = first_name
-        @last_name = last_name
-        @gender = gender
-    end
-    def full_name
-      "#{first_name} #{last_name}"
-    end
-    def doctors
-      "Dr. #{full_name}"
-    end
-    def lawyers
-      "#{full_name}, Esq."
-    end
-end
-
-alex = Person.new("Alex", "Ives", "Male")
-  puts alex.doctors
-  puts alex.lawyers
-  puts alex.gender
+#alex = Person.new("Alex", "Ives", "Male")
+#  puts alex.doctors
+#  puts alex.lawyers
+#  puts alex.gender
 
 #"#{@variable}"
